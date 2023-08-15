eeabb5f789aa00dfcbbcf330fc09c41362845fb3
leon_2192 <leon.strokes@gmail.com>
2023-08-15 17:40:38 -0300

    fix vista

--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
 'use client'
-import Image from 'next/image'
-import Landing from './components/Landing'
 import React, { useEffect } from 'react';
-
+import Landing from './components/Landing';
 
 export default function Home() {
-
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.register("/service-worker.js");
@@ -15,10 +12,10 @@ export default function Home() {
         const installBanner = document.getElementById("install-banner");
 
         setTimeout(() => {
-          installBanner.style.opacity = "1"; // Mostrar el banner después de un retraso
-        }, 3000); // Cambiar a la cantidad de milisegundos que deseas (3 segundos en este caso)
+          installBanner.style.opacity = "1";
+        }, 3000);
 
-        installBanner.style.display = "flex"; // Cambiar display a "flex"
+        installBanner.style.display = "flex";
 
         installBanner.addEventListener("click", () => {
           event.prompt();
@@ -32,34 +29,43 @@ export default function Home() {
         });
       });
     }
-
   }, []);
 
   return (
     <>
-     <div
-          id="install-banner"
-          className="fixed bottom-0 left-0 w-full bg-blue-700 text-white p-2 text-center cursor-pointer flex items-center justify-center"
-          style={{
-            backgroundColor: "#00509e",
-            color: "white",
-            padding: "10px",
-            textAlign: "center",
-            cursor: "pointer",
-            zIndex: "1000",
-            transition: "opacity 0.5s",
-            opacity: "0",
-          }}
-        >
-          <img
-            src="/logosbox.png"
-            alt="Logo SBox"
-            className="w-6 h-6 mr-2"
-            style={{ alignSelf: "center" }} // Añadir esta línea
-          />
-          Instalar esta aplicacion
-        </div>
-    <Landing />
+      <Landing />
+      <div
+        id="install-banner"
+        className="fixed bottom-0 left-0 w-full bg-blue-700 text-white p-2 text-center cursor-pointer flex items-center justify-center"
+        style={{
+          backgroundColor: "#00509e",
+          color: "white",
+          padding: "10px",
+          textAlign: "center",
+          cursor: "pointer",
+          zIndex: "1000",
+          transition: "opacity 0.5s",
+          opacity: "0",
+          display: "none",
+        }}
+      >
+        <img
+          src="/logosbox.png"
+          alt="Logo SBox"
+          className="w-6 h-6 mr-2"
+          style={{ alignSelf: "center" }}
+        />
+        Instalar esta aplicación
+      </div>
+      <style jsx>
+        {`
+          @media (max-width: 767px) {
+            #install-banner {
+              display: flex;
+            }
+          }
+        `}
+      </style>
     </>
-  )
+  );
 }
