eca65612f73a1be80c7730e6f36efef9678d5332
leon_2192 <leon.strokes@gmail.com>
2023-08-16 11:24:17 -0300

    fix banner

--- app/page.js
+++ app/page.js
@@ -1,31 +1,28 @@
 'use client'
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Landing from './components/Landing';
 
 export default function Home() {
+  const [showBanner, setShowBanner] = useState(false);
+
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.register("/service-worker.js");
 
       window.addEventListener("beforeinstallprompt", (event) => {
         event.preventDefault();
-        const installBanner = document.getElementById("install-banner");
 
         setTimeout(() => {
-          installBanner.style.opacity = "1";
+          setShowBanner(true);
         }, 3000);
 
-        installBanner.style.display = "none"; // Ocultar en la carga inicial
-
-        installBanner.addEventListener("click", () => {
-          event.prompt();
-          event.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === "accepted") {
-              console.log("El usuario aceptó instalar la PWA");
-            } else {
-              console.log("El usuario rechazó la instalación de la PWA");
-            }
-          });
+        event.prompt();
+        event.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === "accepted") {
+            console.log("El usuario aceptó instalar la PWA");
+          } else {
+            console.log("El usuario rechazó la instalación de la PWA");
+          }
         });
       });
     }
@@ -34,37 +31,30 @@ export default function Home() {
   return (
     <>
       <Landing />
-      <div
-        id="install-banner"
-        className="fixed bottom-0 left-0 w-full bg-blue-700 text-white p-2 text-center cursor-pointer flex items-center justify-center"
-        style={{
-          backgroundColor: "#00509e",
-          color: "white",
-          padding: "10px",
-          textAlign: "center",
-          cursor: "pointer",
-          zIndex: "1000",
-          transition: "opacity 0.5s",
-          opacity: "0",
-        }}
-      >
-        <img
-          src="/logosbox.png"
-          alt="Logo SBox"
-          className="w-6 h-6 mr-2"
-          style={{ alignSelf: "center" }}
-        />
-        Instalar esta aplicación
-      </div>
-      <style jsx>
-        {`
-          @media (max-width: 900px) {
-            #install-banner {
-              display: flex; /* Mostrar en dispositivos móviles */
-            }
-          }
-        `}
-      </style>
+      {showBanner && (
+        <div
+          id="install-banner"
+          className="fixed bottom-0 left-0 w-full bg-blue-700 text-white p-2 text-center cursor-pointer flex items-center justify-center"
+          style={{
+            backgroundColor: "#00509e",
+            color: "white",
+            padding: "10px",
+            textAlign: "center",
+            cursor: "pointer",
+            zIndex: "1000",
+            transition: "opacity 0.5s",
+            opacity: "0.9",
+          }}
+        >
+          <img
+            src="/logosbox.png"
+            alt="Logo SBox"
+            className="w-6 h-6 mr-2"
+            style={{ alignSelf: "center" }}
+          />
+          Instalar esta aplicación
+        </div>
+      )}
     </>
   );
 }
