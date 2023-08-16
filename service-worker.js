10d5219618253661e7d7125807b13c8662d69020
leon_2192 <leon.strokes@gmail.com>
2023-08-16 11:48:29 -0300

    fix banner test

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,60 @@
-// service-worker.js
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/offline', // Agrega otras rutas que quieras cachear
+];
+
 self.addEventListener('install', (event) => {
-    // Código para la instalación del Service Worker
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener('activate', (event) => {
-    // Código para la activación del Service Worker
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-    // Código para gestionar solicitudes de red (fetch)
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('push', (event) => {
+    const data = JSON.parse(event.data.text());
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.message,
+            icon: '/vercel-192x192.png', // Asegúrate de que la ruta sea correcta
+        })
+    );
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true })
+            .then((clientList) => {
+                if (clientList.length > 0) {
+                    let client = clientList.find((client) => client.focused) || clientList[0];
+                    client.focus();
+                } else {
+                    clients.openWindow('/');
+                }
+            })
+    );
 });
